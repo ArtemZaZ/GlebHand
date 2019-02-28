306df1ac6e32e4ee6a4dc322ddfc795e8d7483d8
ArtemZaZ <temka.911@mail.ru>
2019-02-28 16:32:12 +0300

    переделал под клавиатуру

--- hand.py
+++ hand.py
@@ -2,21 +2,54 @@ import xmlrpc.client
 import joystick
 from config import *
 import time
+from pynput.keyboard import Key, Listener
+import threading
 
 proxy = "http://" + IP + ':' + PORT
 
-J = joystick.Joystick()
-J.open("/dev/input/js0")
-J.start()
-print(J)
-
 client = xmlrpc.client.ServerProxy(proxy)
 
-f1 = 0.0    # глобальные переменные для 5 пальцев
-f2 = 0.0
-f3 = 0.0
-f4 = 0.0
-f5 = 0.0
+finger1 = 0.0  # глобальные переменные для 5 пальцев
+finger2 = 0.0
+finger3 = 0.0
+finger4 = 0.0
+finger5 = 0.0
+
+fingerKeyState1 = False     # состояния клавиш
+fingerKeyState2 = False
+fingerKeyState3 = False
+fingerKeyState4 = False
+fingerKeyState5 = False
+
+
+def on_press(key):
+    print(key)
+    if key == '1':
+        ff1 = True
+    elif key == '2':
+        ff2 = True
+    elif key == '3':
+        ff3 = True
+    elif key == '4':
+        ff4 = True
+    elif key == '5':
+        ff5 = True
+
+
+def on_release(key):
+    if key == '1':
+        ff1 = False
+    elif key == '2':
+        ff2 = False
+    elif key == '3':
+        ff3 = False
+    elif key == '4':
+        ff4 = False
+    elif key == '5':
+        ff5 = False
+    elif key == Key.esc:
+        # Stop listener
+        return False
 
 
 def rerange(f):     # если перегоняет диапазон, вгоняем его в  (0, 1)
@@ -28,40 +61,52 @@ def rerange(f):     # если перегоняет диапазон, вгоня
         return f
 
 
+def threadFunc():
+    # Collect events until released
+    with Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+
+
+threading.Thread(target=threadFunc).start()
+
+
 while True:
-    if J.buttons[FINGER_1_BUTTON]:
-        f1 = f1 + FINGER_SCALE_STEP
+    if fingerKeyState1:
+        finger1 = finger1 + FINGER_SCALE_STEP
     else:
-        f1 = f1 - FINGER_SCALE_STEP
-    f1 = rerange(f1)
+        finger1 = finger1 - FINGER_SCALE_STEP
+    finger1 = rerange(finger1)
 
-    if J.buttons[FINGER_2_BUTTON]:
-        f2 = f2 + FINGER_SCALE_STEP
+    if fingerKeyState2:
+        finger2 = finger2 + FINGER_SCALE_STEP
     else:
-        f2 = f2 - FINGER_SCALE_STEP
-    f2 = rerange(f2)
+        finger2 = finger2 - FINGER_SCALE_STEP
+    finger2 = rerange(finger2)
 
-    if J.buttons[FINGER_3_BUTTON]:
-        f3 = f3 + FINGER_SCALE_STEP
+    if fingerKeyState3:
+        finger3 = finger3 + FINGER_SCALE_STEP
     else:
-        f3 = f3 - FINGER_SCALE_STEP
-    f3 = rerange(f3)
+        finger3 = finger3 - FINGER_SCALE_STEP
+    finger3 = rerange(finger3)
 
-    if J.buttons[FINGER_4_BUTTON]:
-        f4 = f4 + FINGER_SCALE_STEP
+    if fingerKeyState4:
+        finger4 = finger4 + FINGER_SCALE_STEP
     else:
-        f4 = f4 - FINGER_SCALE_STEP
-    f4 = rerange(f4)
+        finger4 = finger4 - FINGER_SCALE_STEP
+    finger4 = rerange(finger4)
 
-    if J.buttons[FINGER_5_BUTTON]:
-        f5 = f5 + FINGER_SCALE_STEP
+    if fingerKeyState5:
+        finger5 = finger5 + FINGER_SCALE_STEP
     else:
-        f5 = f5 - FINGER_SCALE_STEP
-    f5 = rerange(f5)
+        finger5 = finger5 - FINGER_SCALE_STEP
+    finger5 = rerange(finger5)
 
-    client.bendFinger(1, f1)
-    client.bendFinger(2, f2)
-    client.bendFinger(3, f3)
-    client.bendFinger(4, f4)
-    client.bendFinger(5, f5)
+    client.bendFinger(1, finger1)
+    client.bendFinger(2, finger2)
+    client.bendFinger(3, finger3)
+    client.bendFinger(4, finger4)
+    client.bendFinger(5, finger5)
     time.sleep(SEND_DELAY)
+
